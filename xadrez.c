fda35475d7684c95437da1d61ebea763209bf499
lucasthecrea-tor <lucas.andrade2597@gmail.com>
2025-10-29 14:11:05 +0000

    Criei o movimento do BISPO

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    //Mover a Torre 5 casas ára a direita
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); //Imprime a direção do movimento
+
+    // Mover o BISPO 5 Casas na Diagonal (Cima, Direita)
+    int i = 0; // contador começa em 0
+
+    while (i < 5) {
+        printf("Cima, Direita\n"); // Direção do movimento
+        i = i + 1; // soma 1 a cada repetição
     }
+
     return 0;
 }
